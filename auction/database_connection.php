25ce9bd46b01e85f46d95ac6e6a182bd11c11b7c
Gabibi <gabriela.bkva@gmail.com>
2023-11-03 00:50:22 +0000

    Test

--- auction/database_connection.php
+++ auction/database_connection.php
@@ -1,24 +1,51 @@
+<html>
+<body>
 <?php
 // File with re-usable function to establish connection with the database 
 
-// Open connection to database; if connection to database fails, try to re-connect again. If it does not work again for the second time - > redirect user to error message page to increase user experience
-
+// Define function to open connection to database; If connection to database fails, re-connect. IF still unsuccesful, redirect user to error message page
 function open_connection()
 {
     $connection = mysqli_connect("localhost", "AuctionProject", "london2023", "website_auction");
-
     if (!$connection) 
     { 
         $connection = mysqli_connect("localhost", "AuctionProject", "london2023", "website_auction");
-        if ($connection)
-        {
-            return $connection;
-        } 
-        else 
+        if (!$connection)
         {
             header("Location: failed_conncetion.php");
         }
+        else
+        {
+            return $connection;
+        }
     }
+    return $connection;
 }
 
+
+// Define function for making a sql query - adapted from Tutorial 3 slide 7  
+
+function sql_query($connection, $query)
+{
+    $result = mysqli_query($connection, $query);
+    
+    if (!$result)
+    {
+        die('Error making select users query'.mysqli_error($connection));
+    }  
+    return $result;   
+}
+$connection = open_connection();
+$query = "SELECT userfirstname, userlastname FROM Users";
+$result = mysqli_query($connection,$query); //returns mysql object
+echo '<table border="1">';
+while ($row = mysqli_fetch_array($result)) {
+    echo '<tr>';
+    echo '<td>' . $row['userfirstname'] . '</td>';
+    echo '<td>' . $row['userlastname'] . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
 ?>
+</html>
+</body>
