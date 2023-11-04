cc2a52b1d7b98b2c69f57742b90134fdb0e8f1bf
Gabibi <gabriela.bkva@gmail.com>
2023-11-04 12:08:42 +0000

    changes to layout

--- auction/header.php
+++ auction/header.php
@@ -89,20 +89,6 @@
       <!-- Modal body -->
       <div class="modal-body">
         <form method="POST" action="login_result.php">
-        <div class="form-group row">
-          <label for="accountType" class="col-sm-2 col-form-label text-right">Registering as a:</label>
-	        <div class="col-sm-10">
-	          <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="accountType" id="accountBuyer" value="buyer" checked>
-              <label class="form-check-label" for="accountBuyer">Buyer</label>
-            </div>
-          <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="accountType" id="accountSeller" value="seller">
-              <label class="form-check-label" for="accountSeller">Seller</label>
-          </div>
-            <small id="accountTypeHelp" class="form-text-inline text-muted"><span class="text-danger">* Required.</span></small>
-	      </div>
-      </div>
           <div class="form-group">
             <label for="email">Email</label>
             <input type="text" class="form-control" id="email" placeholder="Email">
@@ -114,8 +100,21 @@
           <button type="submit" class="btn btn-primary form-control">Sign in</button>
         </form>
         <div class="text-center">or <a href="register.php">create an account</a></div>
-      </div>
-
+          <div class="form-group row">
+            <label for="accountType" class="col-sm-2 col-form-label text-right">Logging in as a:</label>
+            <div class="col-sm-10">
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="accountType" id="accountBuyer" value="buyer" checked>
+                <label class="form-check-label" for="accountBuyer">Buyer</label>
+              </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="accountType" id="accountSeller" value="seller">
+                <label class="form-check-label" for="accountSeller">Seller</label>
+            </div>
+              <small id="accountTypeHelp" class="form-text-inline text-muted"><span class="text-danger">* Required.</span></small>
+          </div>
+          </div>
+        </div>
     </div>
   </div>
 </div> <!-- End modal -->
