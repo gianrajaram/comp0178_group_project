e66124ddb76c989d6945c480419fd95de3ad3031
acath64 <annecat.xhonneux@me.com>
2023-11-06 12:32:05 +0000

    f

--- auction/database_connection.php
+++ auction/database_connection.php
@@ -64,9 +64,9 @@ function send_query($query)
 
 // Test with INSERT query
 
-//$query = "INSERT INTO Users (userEmail, username, userPassword, userFirstName, userLastName, userAddress, userTel, userGender)".
-//"VALUES ('a@gm.com', 'user121', '111', 'O', 'A', 'Imaginary Land, London E14 9RZ UK', 0792200000, 'Female')";
-//send_query($query);
+$query = "INSERT INTO Users (userEmail, username, userPassword, userFirstName, userLastName, userAddress, userTel, userGender)".
+"VALUES ('a@gm.com', 'user121', '111', 'O', 'A', 'Imaginary Land, London E14 9RZ UK', 0792200000, 'Female')";
+send_query($query);
 
 // Test with Delete query
 
