a2264bf2fac2065c50e55981b8f1c2192f8db4a5
acath64 <annecat.xhonneux@me.com>
2023-11-15 20:56:19 +0000

    mylistings_

--- auction/create_auction_result.php
+++ auction/create_auction_result.php
@@ -140,23 +140,7 @@ $query = "INSERT INTO auctions
 send_query($query);
 
 
-// This function takes the form data and adds the new auction to the database.
 
-/* TODO #1: Connect to MySQL database (perhaps by requiring a file that
-            already does this).  
-            DONE*/
-
-
-/* TODO #2: Extract form data into variables. Because the form was a 'post'
-            form, its data can be accessed via $POST['auctionTitle'], 
-            $POST['auctionDetails'], etc. Perform checking on the data to
-            make sure it can be inserted into the database. If there is an
-            issue, give some semi-helpful feedback to user. */
-
-
-/* TODO #3: If everything looks good, make the appropriate call to insert
-            data into the database. */
-            
 
 // If all is successful, let user know.
 echo('<div class="text-center">Auction successfully created! <a href="mylistings.php">View your new listing.</a></div>');
--- auction/mylistings.php
+++ auction/mylistings.php
@@ -1,4 +1,5 @@
 <?php include_once("header.php")?>
+<?php require_once("database_connection.php")?>
 <?php require("utilities.php")?>
 
 <div class="container">
@@ -6,6 +7,25 @@
 <h2 class="my-3">My listings</h2>
 
 <?php
+//session_start();
+
+$connection = connect();
+
+if(isset($_SESSION['userID'])) {
+  // Get the seller ID
+  $userID = $_SESSION['userID'];
+  //$userID = 2;
+  $query = "SELECT * FROM auctions WHERE sellerID = $userID";
+  $result = send_query($query);
+} else {
+  header("Location: login.php");
+  exit();
+}
+
+
+
+
+
   // This page is for showing a user the auction listings they've made.
   // It will be pretty similar to browse.php, except there is no search bar.
   // This can be started after browse.php is working with a database.
@@ -20,5 +40,37 @@
   // TODO: Loop through results and print them out as list items.
   
 ?>
+
+
+<div class="container my-5">
+
+  <h2 class="my-3">Your Auctions</h2>
+
+  <?php
+  // Check if there are auctions
+  if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+      // Display auction information
+      echo '<div class="card mb-3">';
+      echo '<div class="card-body">';
+      // title is clickable
+      echo '<h5 class="card-title"><a href="listing.php?item_id=' . $row['item_id'] . '">'. $row['auctionName'] . '</a></h5>';
+      echo '<p class="card-text">Start Date: ' . $row['auctionStartDate'] . '</p>';
+      echo '<p class="card-text">End Date: '. $row['auctionEndDate'] . '</p>';
+      echo '</div>';
+      echo '</div>';
+    }
+  } else {
+    echo '<p>No auctions found.</p>';
+  }
+  ?>
+</div>
+
+
+
+
+
+
+
 
 <?php include_once("footer.php")?>
