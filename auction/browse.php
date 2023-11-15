dd7522cd645676a5ce2f6135c2d7589a1614cf01
gianrajaram <gianpremr@gmail.com>
2023-11-15 20:26:12 +0000

    Unfinished WIP

--- auction/browse.php
+++ auction/browse.php
@@ -9,6 +9,9 @@
 <!-- When this form is submitted, this PHP page is what processes it.
      Search/sort specs are passed to this page through parameters in the URL
      (GET method of passing data to a page). -->
+
+
+
 <form method="get" action="browse.php">
   <div class="row">
     <div class="col-md-5 pr-0">
@@ -20,7 +23,7 @@
               <i class="fa fa-search"></i>
             </span>
           </div>
-          <input type="text" class="form-control border-left-0" id="keyword" placeholder="Search for anything">
+          <input type="text" class="form-control border-left-0" id="keyword" placeholder="Search for items">
         </div>
       </div>
     </div>
@@ -56,22 +59,42 @@
 </div>
 
 <?php
+{ #GR - function to sanitise user inputted text
+  function sanitise_input($data) {
+    $data = trim($data); # removes whitespaces from the beginning and end of user input
+    $data = stripslashes($data); #removes backslashes from data
+    $data = htmlspecialchars($data); #
+    return $data;
+  }
+  # you added below 5 options as global variables double check if this is correct way of querying/would cause complications for anyone else.
+  $servername="localhost";
+  $username='AuctionProject';
+  $password='london2023';
+  $DB_name='website_auction';
+  $conn = new mysqli($servername,$username,$password,$DB_name);
+  if ($conn->connect_error) {
+  die('Connection failed: '. $conn->connect_error);
+  }
+}
   // Retrieve these from the URL
   if (!isset($_GET['keyword'])) {
+    $keyword = "";
     // TODO: Define behavior if a keyword has not been specified.
   }
+  # cleaning user input to efficiently query the database and return relevant results
   else {
-    $keyword = $_GET['keyword'];
+    $keyword = sanitise_input($_GET['keyword']); #implemented a function that will clean the data of projected user errors, resulting in a higher probability of a correct match to database
   }
 
   if (!isset($_GET['cat'])) {
+    $category = "All categories";
     // TODO: Define behavior if a category has not been specified.
   }
   else {
     $category = $_GET['cat'];
   }
-  
   if (!isset($_GET['order_by'])) {
+    $ordering = 'pricelow';
     // TODO: Define behavior if an order_by value has not been specified.
   }
   else {
@@ -84,38 +107,74 @@
   else {
     $curr_page = $_GET['page'];
   }
-
-  /* TODO: Use above values to construct a query. Use this query to 
+  
+    /* TODO: Use above values to construct a query. Use this query to 
      retrieve data from the database. (If there is no form data entered,
      decide on appropriate default value/default query to make. */
   
+  ## building a dynamic query based on above conditions:
+  $query = 'SELECT auctionID, auctionName, auctionDescription, auctionCurrentHighestBid, auctionBidCount, auctionEndDate FROM Auctions WHERE 1';
+
+#$keyword search IN NATURAL LANGUAGE MODE -> CURRENTLY DEPENDS ON FULLTEXT MODE BEING ENABLED
+# else: simplify
+####The !empty function in PHP returns true if $keyword has a non-null and non-empty value. This is to ensure that the additional SQL conditions are only appended if there is a keyword to search for.
+  if (!empty($keyword)){
+    $keyword = mysqli_real_escape_string($conn,$keyword);
+    $query .= "AND MATCH (auctionName) AGAINST ('" . $keyword . "' IN NATURAL LANGUAGE MODE) ";
+  }
+  #Category condition:
+  if($category != 'all') {
+    $category = $_GET['cat'];
+    $query .= "AND CategoryClothsType = '" . $category . "' ";
+  }
+
+  #ordering -> expand based on all categories of ordering
+  switch($ordering) {
+    case 'pricelow':
+      $query .= ' ORDER BY auctionCurrentHighestBid ASC ';
+      break;
+    case 'pricehigh':
+      $query .= ' ORDER BY auctionCurrentHighestBid DESC ';
+      break;
+    case 'date':
+      $query .= ' ORDER BY auctionEndDate ASC ';
+      break;
+  }
+
+
+  
+  #$query = 'SELECT auctionID, auctionName,auctionDescription,auctionCurrentHighestBid, auctionBidCount,auctionEndDate FROM Auctions';
+
+
   /* For the purposes of pagination, it would also be helpful to know the
      total number of results that satisfy the above query */
   $num_results = 96; // TODO: Calculate me for real
+  # GR Edit: Calculation = total number of rows in queries_found -> count($queries_found)
   $results_per_page = 10;
+  # GR Edit: leave as is
   $max_page = ceil($num_results / $results_per_page);
+  # GR Edit: Leave as is
 ?>
 
 <div class="container mt-5">
 
 <!-- TODO: If result set is empty, print an informative message. Otherwise... -->
 
-<ul class="list-group">
+<ul class="list-group"> <!------------------------------------------------- LISTINGS -->
 
-<!-- TODO: Use a while loop to print a list item for each auction listing
-     retrieved from the query -->
+<!-- TODO: Use a while loop to print a list item for each auction listing retrieved from the query -->
 
 <?php
   // Demonstration of what listings will look like using dummy data.
   $item_id = "87021";
   $title = "Dummy title";
-  $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget rutrum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat, ipsum vel egestas elementum, sem mi vestibulum eros, et facilisis dui nisi eget metus. In non elit felis. Ut lacus sem, pulvinar ultricies pretium sed, viverra ac sapien. Vivamus condimentum aliquam rutrum. Phasellus iaculis faucibus pellentesque. Sed sem urna, maximus vitae cursus id, malesuada nec lectus. Vestibulum scelerisque vulputate elit ut laoreet. Praesent vitae orci sed metus varius posuere sagittis non mi.";
+  $desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget rutrum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat, ipsum vel egestas elementum, sem mi vestibulum eros, et facilisis dui nisi eget metus. In non elit felis. Ut lacus sem, pulvinar ultricies pretium sed, viverra ac sapien. Vivamus condimentum aliquam rutrum. Phasellus iaculis faucibus pellentesque. Sed sem urna, maximus vitae cursus id, malesuada nec lectus. Vestibulum scelerisque vulputate elit ut laoreet. Praesent vitae orci sed metus varius posuere sagittis non mi.";
   $current_price = 30;
   $num_bids = 1;
   $end_date = new DateTime('2020-09-16T11:00:00');
   
   // This uses a function defined in utilities.php
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+  print_listing_li($item_id, $title, $desc, $current_price, $num_bids, $end_date);
   
   $item_id = "516";
   $title = "Different title";
@@ -125,6 +184,48 @@
   $end_date = new DateTime('2020-11-02T00:00:00');
   
   print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+
+  $item_id = "1313";
+  $title = "Gian Test";
+  $description = "desc";
+  $current_price = 21;
+  $num_bids = 2;
+  $end_date = new DateTime('2020-11-02T00:00:00');
+
+
+  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+
+  #$servername="localhost";
+  #$username='AuctionProject';
+  #$password='london2023';
+  #$DB_name='website_auction';
+  #  $conn = new mysqli($servername,$username,$password,$DB_name);
+  #  if ($conn->connect_error) {
+   #   die('Connection failed: '. $conn->connect_error);
+   # }
+    # SQL query in below else statement pending database change -> auctionName to FULLTEXT (index), this is a pre-req for MATCH ... AGAINST in NATURAL LANGUAGE MODE to work. Query should theoretically be correct.
+   # else{
+  #  $query = 'SELECT auctionID, auctionName,auctionDescription,auctionCurrentHighestBid, auctionBidCount,auctionEndDate FROM Auctions WHERE MATCH($keyword) AGAINST (auctionName) IN NATURAL LANGUAGE MODE';
+   # $resultKeyWord = send_query($query);
+   # $searchSubset = [];
+   # while($rows = mysqli_fetch_assoc($resultKeyWord)) {
+  #    $searchSubset[] = $rows;
+   # }
+
+   # foreach ($searchSubset as $row) {
+  #  $item_id = $row['auctionID'];
+   # $title = $row['auctionName'];
+   # $description = $row['auctionDescription'];
+  #  $current_price = $row['auctionCurrentHighestBid'];
+   # $num_bids = $row['auctionBidCount'];
+   # $end_date = $row['auctionEndDate'];
+   # print_listing_li($item_id,$title,$description,$current_price,$num_bids,$end_date);
+#
+   # }
+   # }
+
+  #  mysqli_close($conn);
+   # }
 ?>
 
 </ul>
