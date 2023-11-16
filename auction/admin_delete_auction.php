d26019263f99a20e1059df5f0da007f4b755e688
Gabibi <gabriela.bkva@gmail.com>
2023-11-16 15:51:19 +0000

    Admin view: Auction deletion

---
+++ auction/admin_delete_auction.php
@@ -0,0 +1,16 @@
+<?php require_once("database_connection.php");
+require_once("utilities.php");
+require_once("header.php")?>
+
+
+<?php
+
+
+// Extract $_POST variable from admin delete user form
+if (isset($_POST['auctionID'])) {
+    $auctionID = $_POST['auctionID'];
+    $query= "DELETE FROM Auctions WHERE auctionID = '$auctionID'";
+    send_query($query);
+    echo "<script>alert('Auction was deleted.');</script>";
+    echo "<script>window.location.href='admin_user_overview.php';</script>";
+}
