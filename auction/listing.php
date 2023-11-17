a2fcf4e48ade9a1d43e46112be072f8c09ab062d
mnamyslowska <mnamyslowska1@gmail.com>
2023-11-17 20:35:16 +0000

    m listing updated

--- auction/listing.php
+++ auction/listing.php
@@ -76,196 +76,189 @@ if ($_SESSION['logged_in']) {
 ?>
 
 <div class="container">
-        <div class="row">
-            <style>
-                .item-img {
-                    width: 50%; /* Adjust the percentage as needed */
-                    height: auto; /* This maintains the aspect ratio */
-                }
-            </style>            
-            <div class="col-sm-6">
-                <div style="margin-top: 20px;">
-                    <img src="images/plainblackTshirt_male.png" class='img-rounded img-responsive item-img'>
-                </div>
-            </div>
-
-
-
-            <div class="col-sm-6">
-            <div style="margin-top: 30px;">
-                <div class="item-title" style="font-size: larger; font-weight: bold;"><?php echo $auctionName ?></div>
-
-
-                <div class="item-seller">
-                    <?php echo "Seller: " . $sellerUsername ?>
-                </div>
-                <div class="item-type">
-                    <?php echo "Type: " . $categoryType ?>
-                </div>
-                <div class="item-color">
-                    <?php echo "Colour: " . $categoryColor ?>
-                </div>
-                <div class="item-gender">
-                    <?php echo "Gender: " . $categoryGender ?>
-                </div>
-                <div class="item-description">
-                    <?php echo "Description: " . $auctionDescription ?>
-                </div>
-                <div class="item-count-bids">
-                    <?php echo "Bid count: " . $auctionBidCount ?>
-                </div>
-                <div class="item-current-price">
-                    <?php echo "Current highest bid: " . $auctionMaxCP ?>
-                </div>
-
-                <div class="item-status">
-                    <?php echo "Status: " . $auctionStatus ?>
-                </div>
-
-                <div style="margin-top: 20px;"> </div>
-                
-                <!-- new watchlist -->
-                <div style="margin-top: 20px;">
-                  <?php if ($auctionStatus == 'Active' && $watching == false): ?>
-                    <form action="watchlist_funcs.php" method="POST">
-                      <input type="hidden" name="auctionID" value="<?php echo $auctionID; ?>">
-                      <input type="hidden" name="userID" value="<?php echo $userID; ?>">
-                      <button type="submit" class="btn btn-primary form-control" name="watchlistsubmit"> Add to the watchlist </button>
-                    </form>
-                  <?php endif; ?>
-                </div>
-
-                <div style="margin-top: 20px;">
-                  <?php if ($auctionStatus == 'Active' && $watching == true): ?>
-                    <?php echo "This auction is already on your watchlist."?>
-                    <form action="watchlist_funcsr.php" method="POST">
-                      <input type="hidden" name="auctionID" value="<?php echo $auctionID; ?>">
-                      <input type="hidden" name="userID" value="<?php echo $userID; ?>">
-                      <button type="submit" class="btn btn-primary form-control" name="watchlistremove"> Remove from the watchlist </button>
-                    </form>
-                  <?php endif; ?>
-                </div>
-
-                <!-- this is for placing bid: -->
-
-                <div style="margin-top: 20px;"> </div>
-                  <?php if ($auctionStatus == 'Active'): ?>
-                    <form action="place_bid.php" method="POST">
-                      <div class="row form-group">
-                        <div class="col-sm-6">
-                          <input type="text" id="bid_price" name="bid_price" placeholder="Enter Your Bid" class="form-control input-frame">
-                          <input type="hidden" name="auctionID" value="<?php echo $auctionID; ?>">
-                          <input type="hidden" name="auctionMaxCP" value="<?php echo $auctionMaxCP; ?>">
-
-                          
-                        </div>
-            
-                        <input type="submit" class="btn btn-default item-button" value="Bid Now" name="submit" id="submit" style="background-color: blue; color: white;"/>
-                      </div>
-                    </form>
-                  <?php endif; ?>
-
-                <div style="margin-top: 20px;"> </div>
-
-                <div class="bid-history">
-                    <!-- Bid History Button -->
-                    <button id="bidHistoryBtn" class="btn btn-primary" style="background-color: gray; color: white;">Bid History</button>
-                </div>
-
-                <!-- Bid History Modal -->
-                <div id="bidHistoryModal" class="modal">
-                    <div class="modal-content">
-                        <span class="close">&times;</span>
-                        <h3>Bid history</h3>
-                        <div id="bidHistoryTable">
-                            <?php if ($bids_result && mysqli_num_rows($bids_result) > 0): ?>
-                                <table>
-                                    <tr>
-                                        
-                                        <th>Bid Value</th>
-                                        <th>Date of Bid</th>
-                                    </tr>
-                                    <?php while ($bid = mysqli_fetch_assoc($bids_result)): ?>
-                                        <tr>
-                                            <td><?php echo htmlspecialchars($bid['bidValue']); ?></td>
-                                            <td><?php echo htmlspecialchars($bid['dateBid']); ?></td>
-                                        </tr>
-                                    <?php endwhile; ?>
-                                </table>
-                            <?php else: ?>
-                                <p>No bids found.</p>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-
-                <style>
-                    .modal {
-                        display: none;
-                        position: fixed;
-                        z-index: 1;
-                        left: 0;
-                        top: 0;
-                        width: 100%;
-                        height: 100%;
-                        overflow: auto;
-                        background-color: rgb(0,0,0);
-                        background-color: rgba(0,0,0,0.4);
-                    }
-                    .modal-content {
-                        background-color: #fefefe;
-                        margin: 15% auto;
-                        padding: 20px;
-                        border: 1px solid #888;
-                        width: 80%;
-                    }
-                    .modal-content table th,
-                    .modal-content table td {
-                        padding: 10px; /* Increase this value for more space */
-                        text-align: left;
-                        border-bottom: 1px solid #ddd;
-                    }
-
-                    .modal-content table {
-                        border-collapse: separate;
-                        border-spacing: 10px 0; /* Adjust horizontal and vertical spacing */
-                    }
-                    .close {
-                        color: #aaa;
-                        float: right;
-                        font-size: 28px;
-                        font-weight: bold;
-                    }
-                    .close:hover,
-                    .close:focus {
-                        color: black;
-                        text-decoration: none;
-                        cursor: pointer;
-                    }
-                </style>
-
-                <script>
-                    var modal = document.getElementById("bidHistoryModal");
-                    var btn = document.getElementById("bidHistoryBtn");
-                    var span = document.getElementsByClassName("close")[0];
-
-                    btn.onclick = function() {
-                        modal.style.display = "block";
-                    }
-
-                    span.onclick = function() {
-                        modal.style.display = "none";
-                    }
-
-                    window.onclick = function(event) {
-                        if (event.target == modal) {
-                            modal.style.display = "none";
-                        }
-                    }
-                </script>
-
+    <div class="row">
+        <style>
+            .item-img {
+                width: 50%; /* Adjust the percentage as needed */
+                height: auto; /* This maintains the aspect ratio */
+            }
+        </style>            
+        <div class="col-sm-6">
+            <div style="margin-top: 20px;">
+                <img src="images/plainblackTshirt_male.png" class='img-rounded img-responsive item-img'>
             </div>
         </div>
+
+
+
+    <div class="col-sm-6">
+        <div style="margin-top: 30px;">
+        <div class="item-details">
+            <h2 class="item-title" style="font-size: 30px; font-weight: bold;"><?php echo $auctionName ?></h2>
+            <p class="item-description" style="font-size: 14px;"><?php echo $auctionDescription ?></p>
+
+            <table class="table table-sm">
+                <tbody>
+                    <tr>
+                        <td>Type: <?php echo $categoryType ?></td>
+                        <td>Colour: <?php echo $categoryColor ?></td>
+                        <td>Gender: <?php echo $categoryGender ?></td>
+                    </tr>
+                </tbody>
+            </table>
+            
+
+            <div class="auction-info">
+                <p>Bid count: <?php echo $auctionBidCount ?></p>
+                <p>Current highest bid: <?php echo $auctionMaxCP ?></p>
+                <p>Status: <?php echo $auctionStatus ?></p>
+            </div>
+        </div> 
+
+        <div style="margin-top: 20px;"> </div>
+        
+        <!-- new watchlist -->
+        <div style="margin-top: 20px;">
+            <?php if ($auctionStatus == 'Active' && $watching == false): ?>
+            <form action="watchlist_funcs.php" method="POST">
+                <input type="hidden" name="auctionID" value="<?php echo $auctionID; ?>">
+                <input type="hidden" name="userID" value="<?php echo $userID; ?>">
+                <button type="submit" class="btn btn-primary form-control" name="watchlistsubmit"> Add to the watchlist </button>
+            </form>
+            <?php endif; ?>
+        </div>
+
+        <div style="margin-top: 20px;">
+            <?php if ($auctionStatus == 'Active' && $watching == true): ?>
+            <?php echo "This auction is already on your watchlist."?>
+            <form action="watchlist_funcsr.php" method="POST">
+                <input type="hidden" name="auctionID" value="<?php echo $auctionID; ?>">
+                <input type="hidden" name="userID" value="<?php echo $userID; ?>">
+                <button type="submit" class="btn btn-primary form-control" name="watchlistremove"> Remove from the watchlist </button>
+            </form>
+            <?php endif; ?>
+        </div>
+
+        <!-- this is for placing bid: -->
+
+        <div style="margin-top: 20px;"> </div>
+            <?php if ($auctionStatus == 'Active'): ?>
+            <form action="place_bid.php" method="POST">
+                <div class="row form-group">
+                <div class="col-sm-6">
+                    <input type="text" id="bid_price" name="bid_price" placeholder="Enter Your Bid" class="form-control input-frame">
+                    <input type="hidden" name="auctionID" value="<?php echo $auctionID; ?>">
+                    <input type="hidden" name="auctionMaxCP" value="<?php echo $auctionMaxCP; ?>">
+
+                    
+                </div>
+    
+                <input type="submit" class="btn btn-default item-button" value="Bid Now" name="submit" id="submit" style="background-color: blue; color: white;"/>
+                </div>
+            </form>
+            <?php endif; ?>
+
+        <div style="margin-top: 20px;"> </div>
+
+        <div class="bid-history">
+            <!-- Bid History Button -->
+            <button id="bidHistoryBtn" class="btn btn-primary" style="background-color: gray; color: white;">Bid History</button>
+        </div>
+
+        <!-- Bid History Modal -->
+        <div id="bidHistoryModal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <h3>Bid history</h3>
+                <div id="bidHistoryTable">
+                    <?php if ($bids_result && mysqli_num_rows($bids_result) > 0): ?>
+                        <table>
+                            <tr>
+                                
+                                <th>Bid Value</th>
+                                <th>Date of Bid</th>
+                            </tr>
+                            <?php while ($bid = mysqli_fetch_assoc($bids_result)): ?>
+                                <tr>
+                                    <td><?php echo htmlspecialchars($bid['bidValue']); ?></td>
+                                    <td><?php echo htmlspecialchars($bid['dateBid']); ?></td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </table>
+                    <?php else: ?>
+                        <p>No bids found.</p>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+
+        <style>
+            .modal {
+                display: none;
+                position: fixed;
+                z-index: 1;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                overflow: auto;
+                background-color: rgb(0,0,0);
+                background-color: rgba(0,0,0,0.4);
+            }
+            .modal-content {
+                background-color: #fefefe;
+                margin: 15% auto;
+                padding: 20px;
+                border: 1px solid #888;
+                width: 80%;
+            }
+            .modal-content table th,
+            .modal-content table td {
+                padding: 10px; /* Increase this value for more space */
+                text-align: left;
+                border-bottom: 1px solid #ddd;
+            }
+
+            .modal-content table {
+                border-collapse: separate;
+                border-spacing: 10px 0; /* Adjust horizontal and vertical spacing */
+            }
+            .close {
+                color: #aaa;
+                float: right;
+                font-size: 28px;
+                font-weight: bold;
+            }
+            .close:hover,
+            .close:focus {
+                color: black;
+                text-decoration: none;
+                cursor: pointer;
+            }
+        </style>
+
+        <script>
+            var modal = document.getElementById("bidHistoryModal");
+            var btn = document.getElementById("bidHistoryBtn");
+            var span = document.getElementsByClassName("close")[0];
+
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        </script>
+
+    </div>
+</div>
 
 
 
