b5fe397ba107189071bb75f4ced69519cf08049b
acath64 <annecat.xhonneux@me.com>
2023-11-22 23:29:48 +0000

    sending message work + design

--- auction/admin_messages.php
+++ auction/admin_messages.php
@@ -1,3 +1,280 @@
 <?php require_once("database_connection.php");
 require_once("utilities.php");
-require_once("header.php")?>
+require_once("header.php");
+
+$query_adminMessage = "SELECT * FROM messagesadmin GROUP BY userID";
+$result_adminMessage = send_query($query_adminMessage);
+
+$connection = connect();
+
+if(isset($_SESSION['userID'])) {
+    // Get the seller ID
+    $senderID = $_SESSION['userID'];
+    //$userID = 2;
+    // check if there are any previous messages
+    $query_received_messages = "SELECT senderID, userID, messageText, COUNT(*) as messageCount FROM messagesadmin WHERE userID = $senderID GROUP BY senderID";
+    $result_received_messages = send_query($query_received_messages);
+    $row_nb = mysqli_fetch_assoc($result_received_messages);
+    $count = $row_nb['messageCount'];
+    $query_lastMessage = "SELECT senderID, messageTime AS latestMessageTime, messageText AS latestMessageText, COUNT(*) as messageCount FROM messagesadmin WHERE userID = 1 AND (senderID, messageTime) IN ( SELECT senderID, MAX(messageTime) AS latestMessageTime FROM messagesadmin WHERE userID = 1 GROUP BY senderID ) GROUP BY senderID, latestMessageTime, latestMessageText ORDER BY latestMessageTime DESC;";
+    $result_lastMessage = send_query($query_lastMessage);
+} else {
+    header("Location: login.php");
+    exit();
+}
+
+
+?>
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overview of Messages</title>
+    <style>
+        .user-info-table th, .user-info-table td {
+            padding: 10px;
+            border-bottom: 1px solid #ccc;
+        }
+
+        
+        .message-container {
+            margin-top: 20px;
+            max-width: 600px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .admin-message {
+            color: #fff;
+            background-color: #007bff;
+        }
+
+        .user-message {
+            color: #000;
+            background-color: #e5e5e5;
+        }
+        
+        .message-box {
+            max-width: 70%; 
+            margin-bottom: 10px; 
+            padding: 0;
+            box-sizing: border-box;
+            border-radius: 5px;
+            border: 5px solid #fff;
+            
+
+            margin: 10px 0;
+            word-wrap: break-word;
+            clear: both;
+        }
+
+        .message-box::after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+
+        .message-box.left {
+            float: left;
+        }
+
+        .message-box.right {
+            float: right;
+        }
+
+        .message-box p {
+            margin: 0;
+        }
+
+        .message-box .admin-message p {
+            color: #fff;
+            margin-bottom: 5px;
+        }
+
+        .message-box .user-message p {
+            color: #000;
+            margin-bottom: 5px;
+        }
+
+        .message-box textarea {
+            width: 100%;
+        }
+
+        .btn-primary {
+            float: right;
+            margin-top: 10px;
+        }
+
+        .message-form {
+            max-width: 600px;
+            margin: 10px auto;
+        }
+
+        .message-textarea {
+            width: 100%; 
+            padding: 10px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            margin-bottom: 10px;
+
+        }
+
+        .message-button button {
+            background-color: #4CAF50; 
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            border-radius: 5px;
+        }
+
+        .message-button {
+            float: right;
+            bottom: 0;
+            right: 0;
+        }
+
+        .container {
+            text-align: center;
+        }
+
+        .message-info-table {
+            width: 70%;
+            margin: 0 auto;
+            border-collapse: collapse;
+        }
+
+        .message-info-table th,
+        .message-info-table td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        .message-info-table th {
+            background-color: #f2f2f2;
+        }
+        
+    </style>
+</head>
+<body>
+
+
+    <?php
+    if (isset($_GET['userID'])) {
+        $targetUserID = $_GET['userID'];
+        $query_admin_messages = "SELECT * FROM messagesadmin WHERE (userID = $senderID AND senderID = $targetUserID) OR (userID = $targetUserID AND senderID = $senderID) ORDER BY messageTime";
+        $result_admin_messages = send_query($query_admin_messages);
+        ?>
+
+
+        <div class="container">
+            <h2>Messages with User ID: <?php echo $targetUserID; ?></h2>
+
+            <?php
+            if (mysqli_num_rows($result_admin_messages) > 0) {
+                echo '<div class="message-container">';
+                while ($row_messages = mysqli_fetch_assoc($result_admin_messages)) {
+                    $messageText = $row_messages["messageText"];
+                    $messageSenderID = $row_messages["senderID"];
+
+
+                    echo '<div class="message-box ' . ($messageSenderID == $senderID ? 'right' : 'left') . '" style="border-color: ' . ($messageSenderID == $targetUserID ?  '#e5e5e5':'#007bff') . ';">';
+                    echo '<p class="' . ($messageSenderID == $targetUserID ? 'user-message' : 'admin-message') . '">' . ($messageSenderID == $targetUserID ? 'You: ' : 'Admin: ') . $messageText . '</p>';
+                    echo '</div>';
+                    
+
+/*
+                    echo '<div class="conversation">';
+                    echo '<p class="admin-message">Admin: Hello, how can I help you</p>';
+
+                    echo '<p class="user-message">User ' . $userID . ': ' . $messages . '</p>';
+                    
+
+                    echo '</div>';
+                    */
+                }
+                
+                echo '<div class="message-box right">';
+                echo '<form method="post" action="">';
+                echo '<textarea id="newMessage" name="newMessage" class="message-textarea" rows="4" placeholder="Type a message..."></textarea>';
+                echo '<div class ="message-button">';
+                echo '<button type="submit">Send Message</button>';
+                echo '</div>';
+                echo '</form>';
+                echo '</div>';
+            } else {
+                echo '<p>No messages found.</p>';
+            }
+        
+            ?>
+
+        </div>
+
+    <?php
+    
+    } else {
+        ?>
+        <div class="container">
+        <h2>All Messages</h2>
+
+        <?php
+        if (mysqli_num_rows($result_received_messages) > 0) {
+            echo '<table class="message-info-table">';
+            echo '<tr><th>Nb of Messages</th><th>User ID</th><th>Last Message</th><th>Time of Latest Message</th></tr></thead><tbody>';
+            
+            if ($result_lastMessage) {
+                while ($row = mysqli_fetch_assoc($result_lastMessage)) {
+                    $latestMessageTime = $row['latestMessageTime'];
+                    $latestMessageText = $row['latestMessageText'];
+                
+                echo '<tr>';
+                echo '<td>'. $count .'</td>';
+                echo '<td><a href="admin_messages.php?userID=' . $row['senderID'] . '">' . $row['senderID'] . '</a></td>';
+                echo '<td>' . $latestMessageText . '</td>';
+                echo '<td>'. $latestMessageTime . '</td>';
+                echo '</tr>';
+                }
+            }
+
+            echo '</tbody></table>';
+        } else {
+            echo '<p>No messages found.</p>';
+        }
+        
+
+    }
+
+    $connection = connect();
+
+
+
+
+    $currentDateTime = date('Y-m-d\TH:i');
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Assuming you're using POST method to submit the form
+        $messageText = $_POST['newMessage'];
+    
+        $query_insert = "INSERT INTO messagesadmin
+                        (messageText, userID, senderID, messageTime)
+                        VALUES
+                        ('$messageText', '$targetUserID', $senderID, '$currentDateTime')";
+        send_query($query_insert);
+    
+        echo '<script>alert("Message sent!");</script>';
+       // echo '<script>window.location.reload();</script>';
+    }
+    ?>
+
+
+
+
+</body>
+</html>  
+
--- auction/contact_admin.php
+++ auction/contact_admin.php
@@ -8,18 +8,105 @@ require_once("header.php")?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Admin</title>
     <style>
-        .conversation {
-            border: 1px solid #ccc;
-            padding: 10px;
+
+        .message-container {
             margin-top: 20px;
+            max-width: 600px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .message-box {
+            max-width: 70%; 
+            margin-bottom: 10px; 
+            padding: 0;
+            box-sizing: border-box;
+            border-radius: 5px;
+            border: 5px solid #fff;
+            
+
+            margin: 10px 0;
+            word-wrap: break-word;
+            clear: both;
         }
 
         .admin-message {
-            color: blue;
+            color: #fff;
+            background-color: #007bff;
         }
 
         .user-message {
-            color: green;
+            color: #000;
+            background-color: #e5e5e5;
+        }
+
+        .message-box::after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+
+        .message-box.left {
+            float: left;
+        }
+
+        .message-box.right {
+            float: right;
+        }
+
+        .message-box p {
+            margin: 0;
+        }
+
+        .message-box .admin-message p {
+            color: #fff;
+            margin-bottom: 5px;
+        }
+
+        .message-box .user-message p {
+            color: #000;
+            margin-bottom: 5px;
+        }
+
+        .message-box textarea {
+            width: 100%;
+        }
+
+        .btn-primary {
+            float: right;
+            margin-top: 10px;
+        }
+
+        .message-form {
+            max-width: 600px;
+            margin: 10px auto;
+        }
+
+        .message-textarea {
+            width: 100%; 
+            padding: 10px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            margin-bottom: 10px;
+
+        }
+
+        .message-button {
+            float: right;
+            bottom: 0;
+            right: 0;
+        }
+
+        .message-button button {
+            background-color: #4CAF50; 
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            border-radius: 5px;
         }
     </style>
     <!-- End of adataption -->
@@ -32,36 +119,52 @@ $connection = connect();
 
 if(isset($_SESSION['userID'])) {
     // Get the seller ID
-    $userID = $_SESSION['userID'];
-    //$userID = 2;
+    $senderID = $_SESSION['userID'];
+    $userID = 1; // the admin
     // check if there are any previous messages
-    $query_hasMessages = "SELECT * FROM `messagesadmin` WHERE userID = $userID";
-    $result_messages = send_query($query_hasMessages);
+    $query_messages = "SELECT * FROM `messagesadmin` WHERE (userID = $userID AND senderID = $senderID) OR (userID = $senderID AND senderID = $userID) ORDER BY messageTime";
+    $result_messages = send_query($query_messages);
 } else {
   header("Location: login.php");
   exit();
 }
 
 if (mysqli_num_rows($result_messages)>0) {
+    echo '<div class="message-container">';
     while($row_messages = mysqli_fetch_assoc($result_messages)) {
-        $messages = $row_messages["messageText"];
+        $messageText = $row_messages["messageText"];
+        $messageSenderID = $row_messages["senderID"];
 
-        echo '<div class="conversation">';
-        echo '<p class="admin-message">Admin: Hello, how can I help you</p>';
-        echo '<p class="user-message">User ' . $userID . ': ' . $messages . '</p>';
+        echo '<div class="message-box ' . ($messageSenderID == $senderID ? 'right' : 'left') . '" style="border-color: ' . ($messageSenderID == $userID ?  '#e5e5e5':'#007bff') . ';">';
+        echo '<p class="' . ($messageSenderID == $userID ? 'user-message' : 'admin-message') . '">' . ($messageSenderID == $senderID ? 'You: ' : 'Admin: ') . $messageText . '</p>';
+        echo '</div>';
+    
 
         //display the messages from the user
-        /*foreach ($messages as $message) {
+        /*
+        echo '<div class="conversation">';
+        echo '<p class="user-message">User ' . $senderID . ': ' . $sentMessages . '</p>';
+        foreach ($messages as $message) {
            echo '<p class="user-message">User ' . $userID . ': ' . $message['messageText'] . '</p>';
         }*/
     }
+    //echo '</div>';
+    
+    echo '<div class="message-box right">';
+    echo '<form method="post" action="">';
+    echo '<textarea id="textMessage" name="textMessage" class="message-textarea" rows="4" placeholder="Type a message..."></textarea>';
+    echo '<div class ="message-button">';
+    echo '<button type="submit">Send Message</button>';
     echo '</div>';
+    echo '</form>';
+    echo '</div>';
+    
 } else {
     // Display the form
     ?>
     <div class="container">
     <!-- send message form -->
-        <div style="max-width: 800px; margin: 10px auto">
+        <div style="max-width: 600px; margin: 10px auto">
             <h2 class="my-3">Send a message!</h2>
             <div class="card">
                 <div class="card-body">
@@ -69,7 +172,7 @@ if (mysqli_num_rows($result_messages)>0) {
 
                         <div class="form-group row">
                             <label for="message" >Your Message: </label>
-                            <div class="col-sm-10">
+                            <div class="col-sm-12">
                                 <textarea class="message" id="textMessage" name = 'textMessage' cols="50" rows="10" required></textarea><br>
                                 <small id="detailsHelp" class="form-text text-muted">Fill in and please be as detailed as possible. We will get back to you as soon as possible.</small>
                             </div>
@@ -94,9 +197,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $messageText = $_POST['textMessage'];
 
     $query_insert = "INSERT INTO messagesadmin
-                    (messageText, userID, messageTime)
+                    (messageText, userID, senderID, messageTime)
                     VALUES
-                    ('$messageText', '$userID', '$currentDateTime')";
+                    ('$messageText', '$userID', $senderID, '$currentDateTime')";
     send_query($query_insert);
 
     echo '<script>alert("Message sent!");</script>';
