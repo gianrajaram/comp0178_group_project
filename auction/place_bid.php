90b9cf4381c9576b8b48d794460d01dcb46182cd
Gabibi <gabriela.bkva@gmail.com>
2023-11-23 15:11:22 +0000

    email - successful bid to bidder

--- auction/place_bid.php
+++ auction/place_bid.php
@@ -20,6 +20,25 @@ if (isset($_POST['bid_price']) && $_POST['bid_price']!='') {
     $bidValue = $_POST['bid_price']; // Assuming the bid value is passed via POST
     $userID = $_SESSION['userID']; // Assuming the user ID is stored in the session
 
+    // Further variables for emailing
+    // bidder email and name
+    $query = "SELECT * FROM Users WHERE userID = '$userID'";
+    $result = send_query($query);
+    $row = mysqli_fetch_assoc($result);
+    $emailBidder = $row['userEmail'];
+    $firstName = $row['userFirstName'];
+
+    // auction titel
+    $query = "SELECT * FROM Auctions WHERE auctionID = '$auctionID'";
+    $result = send_query($query);
+    $row = mysqli_fetch_assoc($result);
+    $auctionTitle = $row['auctionName'];
+    
+
+
+    // previous bidder email
+    // emails of all bidders in watchlist
+
     // Check if user is active; adjust this part according to your application logi
     $dateBid = date('Y-m-d H:i:s');
     if ($bidValue > $auctionMaxCP) {
@@ -30,6 +49,16 @@ if (isset($_POST['bid_price']) && $_POST['bid_price']!='') {
         send_queryMAC($query);
         echo "<script>alert('Your bid was successful.');</script>";
         echo "<script>window.location.href='mybids.php';</script>";
+
+        // send successful bid submission email
+        $name = "ReWear Auctions"; //sender’s name
+        $email = "UCL2023DatabasesAuctionReWear@gmail.com"; //sender’s e-mail address
+        $recipient = $emailBidder; //recipient
+        $mail_body= "$firstName, you successfully bidded £$bidValue on auction $auctionTitle!"; //mail body
+        $subject = "ReWear Auctions - Successful bid submission!"; //subject
+        $header = "From: ". $name . " <" . $email . ">\r\n";
+        mail($recipient, $subject, $mail_body, $header);        
+
     } else {
         echo "<script>alert('Your bid was too low.');</script>";
         echo "<script>window.location.href='listing.php';</script>";
