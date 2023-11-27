1539b3e836013106633e0c95387fe256ea782c4c
Gabibi <gabriela.bkva@gmail.com>
2023-11-27 12:48:16 +0000

    Text edit title to My listings

--- auction/mylistings.php
+++ auction/mylistings.php
@@ -22,25 +22,11 @@ if(isset($_SESSION['userID'])) {
   header("Location: login.php");
   exit();
 }
-
-  // This page is for showing a user the auction listings they've made.
-  // It will be pretty similar to browse.php, except there is no search bar.
-  // This can be started after browse.php is working with a database.
-  // Feel free to extract out useful functions from browse.php and put them in
-  // the shared "utilities.php" where they can be shared by multiple files.
-  
-  
-  // TODO: Check user's credentials (cookie/session).
-  
-  // TODO: Perform a query to pull up their auctions.
-  
-  // TODO: Loop through results and print them out as list items.
   
 ?>
 
 <div class="container my-5">
 
-  <h2 class="my-3">Your Auctions</h2>
 
   <ul class="list-group">
 
